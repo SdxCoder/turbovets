90af95935135f844262273990804bb232fa6576f
saad <chsaadahmed93@gmail.com>
2025-12-05 20:18:25 -0700

    Remove unused mock data, assets, and packages

--- lib/core/utils/asset_names.dart
+++ lib/core/utils/asset_names.dart
@@ -1,10 +1,8 @@
 class AssetNames {
   AssetNames._();
 
-  static const String iconLove = 'assets/icons/love.svg';
   static const String iconMessage = 'assets/icons/message.svg';
   static const String iconSetting = 'assets/icons/setting.svg';
-  static const String iconUser = 'assets/icons/user.svg';
   static const String iconChatEmpty = 'assets/icons/chat_empty.svg';
   static const String iconGroup = 'assets/icons/group.svg';
 }
